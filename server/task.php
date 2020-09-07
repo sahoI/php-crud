50888ec80117e1b77bde678a474bf9429904c498
伊神紗帆 <>
2020-09-08 00:33:46 +0900

    微修正

--- server/task.php
+++ server/task.php
@@ -2,13 +2,12 @@
     function getTasks($db) {
         $sql = "SELECT * FROM task";
         $result = $db->query($sql);
-        if($result) {
-            foreach ($result as $task) {
-                $id = $task["id"];
-                $array[$id]["title"] = $task['title'];
-                // $array[$id]["description"] = $task['description'];
-            }
-            echo json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        foreach ($result as $task) {
+            $id = $task["id"];
+            $array[$id]["title"] = $task['title'];
+        }
+        if($array) {
+            echo json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . PHP_EOL;
         } else {
             echo "データがありません。" . PHP_EOL;
         }
@@ -16,13 +15,10 @@
     function createTask($db) {
         if(!empty($_POST)) {
             $title = $_POST['title'];
-            // $description = $_POST['description'];
 
             try {
                 $sql = "INSERT INTO task (title) VALUES ('$title')";
-                // $sql = "INSERT INTO task (title, description) VALUES ('$title', '$description')";
                 $result = $db->query($sql);
-
                 if ($result) {
                     unset($_POST);
                     echo "作成しました。" . PHP_EOL;
@@ -34,32 +30,27 @@
             }
         }
     }
-    function getTask($db,$id)
-    {
+    function getTask($db,$id) {
         $sql = "SELECT * FROM task WHERE id=$id";
         $result = $db->query($sql);
         if($result) {
             foreach ($result as $task) {
                 $id = $task["id"];
                 $array[$id]["title"] = $task['title'];
-                // $array[$id]["description"] = $task['description'];
-                echo json_encode($array, JSON_UNESCAPED_UNICODE);
+                echo json_encode($array, JSON_UNESCAPED_UNICODE) . PHP_EOL;
             }
         } else {
             echo "データがありません。" . PHP_EOL;
-        }
+        } 
     }
 
-    function updateTask($db,$id)
-    {
+    function updateTask($db,$id) {
         parse_str(file_get_contents('php://input'), $put_param);
         if(!empty($put_param))
         {
             $title = $put_param['title'];
-            // $description = $put_param['description'];
             try {
                 $sql = "UPDATE task SET title='$title' WHERE id=$id";
-                // $sql = "UPDATE task SET title='$title', description='$description' WHERE id=$id";
                 $result = $db->query($sql);
                 echo '変更しました' . PHP_EOL;
             } catch (PDOException $e) {
@@ -70,8 +61,7 @@
         }
     }
 
-    function deleteTask($db,$id)
-    {
+    function deleteTask($db,$id) {
         if(!empty($id)) {
             $sql = "DELETE FROM task WHERE id=$id";
             $result = $db->query($sql);
