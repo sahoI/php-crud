f25325ad010e4948d71acd7445138028baf3a2a8
伊神紗帆 <>
2020-09-08 02:32:39 +0900

    Create setup.sh.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+touch docker-compose.yml 
+
+mkdir -p docker/nginx 
+touch docker/nginx/nginx.conf
+
+mkdir -p docker/php 
+touch docker/php/Dockerfile 
+touch docker/php/php.ini 
+
+mkdir server 
+touch server/index.php
